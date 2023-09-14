f42d946e9dc0ec49f0a4ef67b2d5b780c5829435
Dima <dumitruwebdev@gmail.com>
2023-09-14 10:07:39 +0300

    master

--- js/nav-bar.js
+++ js/nav-bar.js
@@ -3,24 +3,24 @@ document.addEventListener("DOMContentLoaded", function () {
   window.addEventListener("resize", resizeNav);
 
   function resizeNav() {
-    const thisNav = document.querySelector(".current-menu-item").offsetLeft;
-    const menuItems = document.querySelectorAll(".menu-item");
+    const thisNav = document.querySelector(".header-inner .current-menu-item").offsetLeft;
+    const menuItems = document.querySelectorAll(".header-inner .menu-item");
 
     menuItems.forEach(function (item) {
       item.addEventListener("mouseover", function () {
         const left = this.offsetLeft - thisNav;
         const width = this.offsetWidth;
         const initWidth =
-          document.querySelector(".current-menu-item").offsetWidth;
-        document.querySelector(".wee").style.left = left + "px";
-        document.querySelector(".wee").style.width = width + "px";
+          document.querySelector(".header-inner .current-menu-item").offsetWidth;
+        document.querySelector(".header-inner .wee").style.left = left + "px";
+        document.querySelector(".header-inner .wee").style.width = width + "px";
       });
 
       item.addEventListener("mouseout", function () {
         const initWidth =
-          document.querySelector(".current-menu-item").offsetWidth;
-        document.querySelector(".wee").style.left = "0";
-        document.querySelector(".wee").style.width = initWidth + "px";
+          document.querySelector(".header-inner .current-menu-item").offsetWidth;
+        document.querySelector(".header-inner .wee").style.left = "0";
+        document.querySelector(".header-inner .wee").style.width = initWidth + "px";
       });
     });
   }
