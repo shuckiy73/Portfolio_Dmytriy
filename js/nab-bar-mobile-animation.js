fbe38f23073c72632a95463356fea54bdb73e426
Dima <dumitruwebdev@gmail.com>
2023-09-15 13:43:49 +0300

    update

--- js/nab-bar-mobile-animation.js
+++ js/nab-bar-mobile-animation.js
@@ -1,58 +1,4 @@
-$(document).ready(function() {
-    var paperMenu = {
-      $window: $('#paper-window'),
-      $paperFront: $('#paper-front'),
-      $hamburger: $('.hamburger'),
-      offset: 1800,
-      pageHeight: $('#paper-front').outerHeight(),
-    
-      open: function() {
-        this.$window.addClass('tilt');
-        this.$hamburger.off('click');
-        $('#container, .hamburger').on('click', this.close.bind(this));
-        this.hamburgerFix(true);
-        console.log('opening...');
-      },
-      close: function() {
-        this.$window.removeClass('tilt'); 
-        $('#container, .hamburger').off('click');
-        this.$hamburger.on('click', this.open.bind(this));
-        this.hamburgerFix(false);
-        console.log('closing...');
-      },
-      updateTransformOrigin: function() {
-        scrollTop = this.$window.scrollTop();
-        equation = (scrollTop + this.offset) / this.pageHeight * 100;
-        this.$paperFront.css('transform-origin', 'center ' + equation + '%');
-      },
-      hamburgerFix: function(opening) {
-        if(opening) {
-          $('.hamburger').css({
-            position: 'absolute',
-            top: this.$window.scrollTop() + 14.5 + 'px'
-          });
-        } else {
-          setTimeout(function() {
-            $('.hamburger').css({
-              position: 'fixed',
-              top: '14.5px'
-            });
-          }, 300);
-        }
-      },
-      bindEvents: function() {
-        this.$hamburger.on('click', this.open.bind(this));
-        $('.close').on('click', this.close.bind(this));
-        this.$window.on('scroll', this.updateTransformOrigin.bind(this));
-      },
-      init: function() {
-        this.bindEvents();
-        this.updateTransformOrigin();
-      },
-    };
-  
-    paperMenu.init();
-});
+
 
 const headerHeight = document.querySelector('header.header').clientHeight;
 const hamburger = document.querySelector('.hamburger');
@@ -70,4 +16,60 @@ window.addEventListener('resize', () => {
   let topPosition = (headerHeight - hamburger.clientHeight) / 2;
   hamburger.style.top = `${topPosition}px`;
   closeHamburger.style.top = `${topPosition}px`;
+});
+
+$(document).ready(function(topPosition) {
+  var paperMenu = {
+    $window: $('#paper-window'),
+    $paperFront: $('#paper-front'),
+    $hamburger: $('.hamburger'),
+    offset: 1800,
+    pageHeight: $('#paper-front').outerHeight(),
+  
+    open: function() {
+      this.$window.addClass('tilt');
+      this.$hamburger.off('click');
+      $('#container, .hamburger').on('click', this.close.bind(this));
+      this.hamburgerFix(true);
+      console.log('opening...');
+    },
+    close: function() {
+      this.$window.removeClass('tilt'); 
+      $('#container, .hamburger').off('click');
+      this.$hamburger.on('click', this.open.bind(this));
+      this.hamburgerFix(false);
+      console.log('closing...');
+    },
+    updateTransformOrigin: function() {
+      scrollTop = this.$window.scrollTop();
+      equation = (scrollTop + this.offset) / this.pageHeight * 100;
+      this.$paperFront.css('transform-origin', 'center ' + equation + '%');
+    },
+    hamburgerFix: function(opening) {
+      if(opening) {
+        $('.hamburger').css({
+          position: 'absolute',
+          top: this.$window.scrollTop() + topPosition + 'px'
+        });
+      } else {
+        setTimeout(function() {
+          $('.hamburger').css({
+            position: 'fixed',
+            top: topPosition + 'px'
+          });
+        }, 300);
+      }
+    },
+    bindEvents: function() {
+      this.$hamburger.on('click', this.open.bind(this));
+      $('.close').on('click', this.close.bind(this));
+      this.$window.on('scroll', this.updateTransformOrigin.bind(this));
+    },
+    init: function() {
+      this.bindEvents();
+      this.updateTransformOrigin();
+    },
+  };
+
+  paperMenu.init();
 });
