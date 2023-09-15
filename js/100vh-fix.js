67b6ebbf39272c5bf9c4caa2a79bf5aaa9a052f2
Dima <dumitruwebdev@gmail.com>
2023-09-15 15:13:13 +0300

    master

--- js/100vh-fix.js
+++ js/100vh-fix.js
@@ -5,3 +5,39 @@ const documentHeight = () => {
 
 window.addEventListener('resize', documentHeight);
 documentHeight();
+
+let dir;
+function scrollDirecrionChecker(offset) {
+  if (!lastOffset) {
+    lastOffset = offset;
+    return;
+  }
+  if (offset.y < lastOffset.y) {
+    dir = 'up';
+  } else if (offset.y > lastOffset.y) {
+    dir = 'down';
+  } else {
+    dir = 'still';
+  }
+
+  lastOffset = offset;
+
+  switch (dir) {
+    case 'up':
+    case 'down':
+    case 'still':
+  }
+}
+
+scrollbar.addListener(function (status) {
+    let offset = status.offset;
+    scrollDirecrionChecker(offset);
+    if (dir == 'down') {
+      let nViewH = window.outerHeight;
+      nViewH -= 250;
+      $('body').css('height', nViewH + 'px');
+      window.scrollTo(0, 1);
+    } else if (dir == 'up') {
+      window.scrollTo(0, 0);
+    }
+  });
