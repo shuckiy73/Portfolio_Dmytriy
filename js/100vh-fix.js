aaf1a91a90a0784d3735a2305fb7d3128d7e667f
Dima <dumitruwebdev@gmail.com>
2023-09-15 14:39:17 +0300

    master

--- js/100vh-fix.js
+++ js/100vh-fix.js
@@ -1,8 +1,8 @@
-const documentHeight = () => {
-    const doc = document.documentElement;
-    const setHeight = window.innerHeight * (1 / 10);
-    doc.style.setProperty('--doc-height', `${setHeight}px`);
-};
+// const documentHeight = () => {
+//     const doc = document.documentElement;
+//     const setHeight = window.innerHeight * (1 / 10);
+//     doc.style.setProperty('--doc-height', `${setHeight}px`);
+// };
 
-window.addEventListener('resize', documentHeight);
-documentHeight();
+// window.addEventListener('resize', documentHeight);
+// documentHeight();
