2713b1da7b2d3d964e0e635005db5ae024de887b
Dima <dumitruwebdev@gmail.com>
2023-09-15 15:52:46 +0300

    master

--- js/100vh-fix.js
+++
@@ -1,7 +0,0 @@
-// const documentHeight = () => {
-//     const doc = document.documentElement;
-//     doc.style.setProperty('--doc-height', `${window.innerHeight}px`);
-// };
-
-// window.addEventListener('resize', documentHeight);
-// documentHeight();
